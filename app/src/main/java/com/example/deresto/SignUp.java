e55fe87b1d7241645bc7e653c1d642f39d25351c
septianidn <eltianhost@gmail.com>
2021-10-05 05:42:23 +0700

    Menambahkan activity home

--- app/src/main/java/com/example/deresto/SignUp.java
+++ app/src/main/java/com/example/deresto/SignUp.java
@@ -6,21 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void kembali_landing(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     public void login(View view) {
         Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
-    }
-
-    public void signup(View view) {
-        Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
     }
 }
